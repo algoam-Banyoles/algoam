7d1ee9ed2b432a466f30cf3732d4cfd87cbe461d
Albert <algoam@gmail.com>
2025-06-16 13:29:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuration for the Multi YouTube Viewer
 window.APP_CONFIG = {
-  API_KEY: '',
+  API_KEY: 'AIzaSyAgQNSOrxd5EQYZTbLpY63mcafFOP519Jo',
   // Base URL for the CORS proxy used in canal.js
   CORS_PROXY: 'https://corsproxy.io/?'
 };
